51d81ce9ae315e9d45fcd4463d844943a1ff9274
Originem <553816216@qq.com>
2018-12-19 18:58:47 +0800

    add ships, complete many things

---
+++ src/data/scripts/weapons/CMC_AphroditeEveryFrameEffect.java
@@ -0,0 +1,45 @@
+package data.scripts.weapons;
+
+import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.EveryFrameWeaponEffectPlugin;
+import com.fs.starfarer.api.combat.WeaponAPI;
+import com.fs.starfarer.api.impl.campaign.fleets.FleetFactoryV3;
+import com.fs.starfarer.api.loading.MuzzleFlashSpec;
+import org.lazywizard.lazylib.MathUtils;
+
+public class CMC_AphroditeEveryFrameEffect implements EveryFrameWeaponEffectPlugin {
+    private static final float MAX_INCREASE_FACTOR = 0.75f;
+    private static final float CHARGE_UP_NEED_TIME = 2f;
+    private static final float CHARGE_DOWN_NEED_TIME = 1.5f;
+
+    private float increaseFactor = 0;
+    private boolean init = false;
+    private MuzzleFlashSpec originMuzzleFlash;
+
+    @Override
+    public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
+        if (weapon.getShip().getOriginalOwner() == -1 || weapon.getShip().isHulk()) {
+            return;
+        }
+        if (!init) {
+            init = true;
+            originMuzzleFlash = weapon.getMuzzleFlashSpec().clone();
+            weapon.ensureClonedSpec();
+        }
+        float coolDownRemain = weapon.getCooldownRemaining();
+        if (!weapon.isFiring() && weapon.getChargeLevel() <= 0) {
+            increaseFactor -= amount / CHARGE_DOWN_NEED_TIME;
+        } else {
+            increaseFactor += amount / CHARGE_UP_NEED_TIME;
+        }
+        increaseFactor = MathUtils.clamp(increaseFactor, 0f, 1f);
+
+        if (coolDownRemain > 0) {
+            coolDownRemain -= increaseFactor * amount * MAX_INCREASE_FACTOR;
+            coolDownRemain = MathUtils.clamp(coolDownRemain, 0f, 1f);
+            weapon.setRemainingCooldownTo(coolDownRemain);
+        }
+
+        weapon.getMuzzleFlashSpec().setLength(originMuzzleFlash.getLength() * (1f + increaseFactor * MAX_INCREASE_FACTOR));
+    }
+}
---
+++ src/data/scripts/weapons/CMC_OIPOnHit.java
@@ -0,0 +1,51 @@
+package data.scripts.weapons;
+
+import com.fs.starfarer.api.combat.*;
+import org.lazywizard.lazylib.MathUtils;
+import org.lwjgl.util.vector.Vector2f;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CMC_OIPOnHit implements OnHitEffectPlugin {
+    private static final float MIN_DISTANCE = 100f;
+
+    @Override
+    public void onHit(DamagingProjectileAPI projectile, CombatEntityAPI target, Vector2f point, boolean shieldHit, CombatEngineAPI engine) {
+        if (shieldHit) return;
+        if (target.getHitpoints() <= 0) {
+            engine.spawnExplosion(point, new Vector2f(), Color.white, 100f, 0.2f);
+            CombatEntityAPI empTarget = null;
+            float minDis = MIN_DISTANCE;
+            float dis;
+            List<CombatEntityAPI> entities = new ArrayList<>();
+            entities.addAll(engine.getShips());
+            entities.addAll(engine.getMissiles());
+            for (CombatEntityAPI entity : entities) {
+                if (entity.getOwner() == projectile.getOwner()) continue;
+                if (entity == target) continue;
+                dis = MathUtils.getDistance(entity, point);
+                if (dis < minDis) {
+                    minDis = dis;
+                    empTarget = entity;
+                }
+            }
+            if (empTarget != null) {
+                engine.spawnEmpArcPierceShields(
+                        projectile.getSource(),
+                        point,
+                        null,
+                        empTarget,
+                        DamageType.ENERGY,
+                        projectile.getDamageAmount(),
+                        projectile.getDamageAmount() / 2f,
+                        10000f,
+                        "tachyon_lance_emp_impact",
+                        3f,
+                        Color.white, Color.white);
+            }
+        }
+    }
+}
